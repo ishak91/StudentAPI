e071b3897fb868fa66188485b7c34623cac18388
ahamed-ishak <46317157+ahamed-ishak@users.noreply.github.com>
2021-04-27 11:02:01 +0530

    inital calss for student api

---
+++ StudentAPI/Services/StudentService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentAPI.DataAccess;
+using StudentAPI.Model;
+
+namespace StudentAPI.Services
+{
+    public interface IStudentService
+    {
+        public List<Student> GetAll();
+        public Student GetById(int studentId);
+    }
+    public class StudentService : IStudentService
+    {
+        public List<Student> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetById(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+   
+}
