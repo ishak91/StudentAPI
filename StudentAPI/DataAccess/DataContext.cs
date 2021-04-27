803231cd3de869c7cdd5aa2cfdee9278e96a8e3f
ahamed-ishak <46317157+ahamed-ishak@users.noreply.github.com>
2021-04-27 10:20:30 +0530

    add data context

---
+++ StudentAPI/DataAccess/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentAPI.Services;
+
+namespace StudentAPI.DataAccess
+{
+    public class DataContext
+    {
+
+        private List<Student> _studentList = new List<Student>
+        {
+            new Student() {Id = 1, City = "Colombo", FirstName = "Kid", LastName = "Lupus"},
+            new Student() {Id = 2, City = "Kandy", FirstName = "Roger ", LastName = "Flopple"},
+            new Student() {Id = 3, City = "Negambo", FirstName = "Flip", LastName = "Jupiter"},
+            new Student() {Id = 4, City = "Jaffna", FirstName = "Silk", LastName = "Teddy"},
+            new Student() {Id = 5, City = "Gampaha", FirstName = "Max", LastName = "Colorado"},
+
+
+        };
+
+
+        public IEnumerable<Student> Student => _studentList;
+    }
+}
--- StudentAPI/Startup.cs
+++ StudentAPI/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StudentAPI.DataAccess;
 
 namespace StudentAPI
 {
@@ -26,7 +27,7 @@ namespace StudentAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddTransient<DataContext>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
